f691ed88d888198846980707f1b3aea5fe043963
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-04 21:32:23 -0600

    Started a JFrame window with the javax.swing library

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.mavenproject1;
+
+import javax.swing.*;
+
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        JFrame Mywindow;
+        Mywindow = new JFrame();
+        Mywindow.setSize(300,200);
+        Mywindow.setTitle("A brief welcome to our first program!");
+        Mywindow.setVisible(true);
+        Mywindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+    }
+}
