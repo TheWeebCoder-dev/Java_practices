d46a29ad0ccf122746a8159684d692862d352866
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-30 12:31:02 -0600

    practicing casting in java

---
+++ src/main/java/Casting.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Cpedr
+ */
+public class Casting {
+public static void main(String args[]){
+double pi = 3.1416;
+int firstDigit = (int)pi;
+
+System.out.print(firstDigit);
+    
+}     
+}
