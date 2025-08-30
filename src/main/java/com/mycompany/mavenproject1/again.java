8dbb9b9300f3c8669bf5427c18db0198019e7219
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-30 12:08:15 -0600

    practicing java again

---
+++ src/main/java/com/mycompany/mavenproject1/again.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+public class again {
+    
+public static void main(String args[]){
+
+int Integer = 10;
+float floating = 1.0f;
+char character = 'a';
+boolean bool = true;
+String text = "Hello";
+    
+System.out.println(text + "world");
+}   
+
+}
