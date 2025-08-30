20c2298580ef1084a6e93205b7531bab258ceb75
TheWeebCoder-TH07 <theweebcoder@gmail.com>
2025-08-30 12:40:25 -0600

    testing min,max and random functions

---
+++ src/main/java/Test.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Cpedr
+ */
+public class Test {
+public static void main(String args[]){
+
+int x = 10;
+int y = 20;
+
+System.out.println(Math.max(x,y));
+System.out.print(Math.min(x, y));
+System.out.print(Math.random());
+
+
+}    
+}
